3930a4aaaa224bde3e2448d47a1dc62685e2d01b
Francesco De Carlo <decarlof@gmail.com>
2015-07-17 17:36:21 -0500

    first commit

--- rec_APS_1ID.py
+++ rec_APS_1ID.py
@@ -3,7 +3,7 @@ import tomopy
 import numpy as np
 import h5py
 
-def rec_test(file_name, sino_start, sino_end):
+def rec_test(file_name, sino_start, sino_end, best_center, output_name):
 
     print '\n#### Processing '+ file_name
     print "Test reconstruction of slice [%d]" % sino_start
@@ -16,7 +16,8 @@ def rec_test(file_name, sino_start, sino_end):
     
     # normalize the prj
     prj = tomopy.normalize(prj, flat, dark)
-    calc_center = tomopy.find_center(prj, theta, emission=False, init=1024, ind=0, tol=0.3)
+    calc_center = tomopy.find_center(prj, theta, emission=False, init=best_center, ind=0, tol=0.3)
+    print best_center, calc_center
 
     # reconstruct 
     rec = tomopy.recon(prj, theta, center=calc_center, algorithm='gridrec', emission=False)
@@ -26,21 +27,23 @@ def rec_test(file_name, sino_start, sino_end):
 
     print "Slice saved as [%s_00000.tiff]" % output_name
     
-##################################### Inputs ##########################################################
-file_name = '/local/dataraid/databank/templates/dataExchange/tmp/APS_1ID_bov_ver6_0N.h5' 
-output_name = '/local/dataraid/databank/templates/dataExchange/tmp/rec/APS_1ID_bov_ver6_0N_rec'
-best_center = 985.6; sino_start = 1200; sino_end = 1202; 
-rec_test(file_name, sino_start, sino_end)
+def main():
+    ##################################### Inputs ##########################################################   
+    file_name = '/local/dataraid/databank/templates/dataExchange/tmp/APS_1ID_bov_ver6_0N.h5' 
+    output_name = '/local/dataraid/databank/templates/dataExchange/tmp/rec/rec_bov_ver6_0N_'
+    best_center = 976.6; sino_start = 1200; sino_end = 1202; 
 
-file_name = '/local/dataraid/databank/templates/dataExchange/tmp/APS_1ID_bov_ver6_20N.h5' 
-output_name = '/local/dataraid/databank/templates/dataExchange/tmp/rec/APS_1ID_bov_ver6_20N_rec'
-best_center = 995.4; sino_start = 1200; sino_end = 1202; 
-if reconstruction_test: rec_test(file_name, sino_start, sino_end)
-else: rec_full(file_name, sino_start, sino_end)
+#    file_name = '/local/dataraid/databank/templates/dataExchange/tmp/APS_1ID_bov_ver6_20N.h5' 
+#    output_name = '/local/dataraid/databank/templates/dataExchange/tmp/rec/rec_bov_ver6_20N_'
+#    best_center = 995.4; sino_start = 1200; sino_end = 1202; 
 
-file_name = '/local/dataraid/databank/templates/dataExchange/tmp/APS_1ID_bov_ver6_fail.h5' 
-output_name = '/local/dataraid/databank/templates/dataExchange/tmp/rec/APS_1ID_bov_ver6_fail_rec'
-best_center = 998.4; sino_start = 1200; sino_end = 1202; 
-rec_test(file_name, sino_start, sino_end)
+#    file_name = '/local/dataraid/databank/templates/dataExchange/tmp/APS_1ID_bov_ver6_fail.h5' 
+#    output_name = '/local/dataraid/databank/templates/dataExchange/tmp/rec/rec_bov_ver6_fail_'
+#    best_center = 998.4; sino_start = 1200; sino_end = 1202; 
+
+    rec_test(file_name, sino_start, sino_end, best_center, output_name)
+
+if __name__ == "__main__":
+    main()
 
 
