8456c65b1b1b7d10218c54d3eef7fc009ab7be0a
Francesco De Carlo <decarlof@gmail.com>
2015-08-03 21:30:25 -0500

    first commit

--- performance_rec_full.py
+++ performance_rec_full.py
@@ -6,12 +6,24 @@ import sys, getopt
 
 def main(argv):
     try:        opts, args = getopt.getopt(argv,"hc:s:",["core=","sino="])    except getopt.GetoptError:        print 'test.py -c <ncore> -s <nsino>'        sys.exit(2)    for opt, arg in opts:        if opt == '-h':            print 'test.py -c <ncore> -s <nsino>'            sys.exit()        elif opt in ("-c", "--core"):            ncore = int(arg)        elif opt in ("-s", "--sino"):            nsino = int(arg)
+
+    # **********************************************
     file_name = '/local/decarlo/data/proj_10.hdf'
     output_name = './recon/proj10_rec'
     sino_start = 0
-    sino_end = 2160
+    sino_end = 2048
+    # **********************************************
+    file_name = '/local/decarlo/data/Hornby_APS_2011.h5'
+    output_name = './recon/Hornby_APS_2011_'
+    best_center=1024
+    sino_start = 0
+    sino_end = 1792
+    # **********************************************
 
     step_00 = time.time()
+    step_02_delta_total = 0
+    
+    count = 0
     while (sino_start <= (sino_end - nsino)):
         # Read HDF5 file.
         prj, flat, dark = tomopy.io.exchange.read_aps_32id(file_name, sino=(sino_start, sino_start+nsino))
@@ -33,12 +45,16 @@ def main(argv):
 
         step_02 = time.time()
         step_02_delta = step_02 - step_01
-        print prj.shape[1], prj.shape[2], prj.shape[0], ncore, str(datetime.timedelta(seconds=int(step_02_delta))), int(step_02_delta)
-        #tomopy.io.writer.write_tiff_stack(rec, fname=output_name)        
-        sino_start = sino_start + nsino
+        #print prj.shape[1], prj.shape[2], prj.shape[0], ncore, str(datetime.timedelta(seconds=int(step_02_delta))), int(step_02_delta)
+        #tomopy.io.writer.write_tiff_stack(rec, fname=output_name)   
+        step_02_delta_total += step_02_delta     
+        sino_start += nsino
+        count += 1
     step_03 = time.time()    
     step_03_delta = step_03 - step_00
-    print "*", prj.shape[1], prj.shape[2], prj.shape[0], ncore, str(datetime.timedelta(seconds=int(step_03_delta))), int(step_03_delta)
+    print count * prj.shape[1], prj.shape[2], prj.shape[0], ncore, str(datetime.timedelta(seconds=int(step_02_delta_total))), int(step_02_delta_total)
+    print count * prj.shape[1], prj.shape[2], prj.shape[0], ncore, str(datetime.timedelta(seconds=int(step_03_delta))), int(step_03_delta)
+    print count * prj.shape[1], prj.shape[2], prj.shape[0], ncore, str(datetime.timedelta(seconds=int(step_03_delta - step_02_delta_total))), int(step_03_delta - step_02_delta_total)
 
 if __name__ == "__main__":   main(sys.argv[1:])
    
